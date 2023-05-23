7ad8b67cc039bd86ae073b9fa4c88ba31003a26f
vincenzodivita <divitavincenzo@icloud.com>
2023-05-23 17:04:16 +0200

    Add main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script>
   import HeaderApp from './components/HeaderApp.vue';
+  import MainApp from './components/MainApp.vue';
   import FooterApp from './components/FooterApp.vue';
   export default {
     components: {
       HeaderApp,
       FooterApp,
+      MainApp,
     }
   }
 </script>
 
 <template>
   <HeaderApp/>
+
+  <MainApp />
 
   <FooterApp/>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+		thumb: String,
+		price: String,
+		series: String,
+		type: String,
+	},
+
+}
+</script>
+
+
+
+<template>
+  <div class="card">
+    <img :src="thumb" alt="series">
+    <h4>{{ series.toUpperCase() }} </h4>
+</div>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
+
+.card{
+    height: 150px;
+    width: 150px;
+    margin-top: 4em;
+    img{
+        max-height: 100%;
+        aspect-ratio: 1/1;
+        box-shadow: 5px 10px rgb(36, 36, 36);
+    } 
+    h4{
+        margin-top: 1em;
+        color: $base-color;
+    }
+}
+
+</style>
